520f0a932e48e4420a0f347a206aa67118045464
Codehardt <marcel.gebhardt@bsk-consulting.de>
2020-08-18 09:38:05 +0200

    upgraded aho-corasick, removed testify as dep

--- evalostic_test.go
+++ evalostic_test.go
@@ -2,13 +2,30 @@ package evalostic
 
 import (
 	"fmt"
+	"runtime"
 	"testing"
-
-	testify "github.com/stretchr/testify/assert"
 )
 
+func assert(t *testing.T, b bool) {
+	_, f, l, _ := runtime.Caller(1)
+	if !b {
+		t.Fatalf("assertion failed in %s:%d", f, l)
+	}
+}
+
+func sameIntegers(a []int, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func TestEvalostic(t *testing.T) {
-	assert := testify.New(t)
 	e, err := New([]string{
 		`"foo" OR "bar"`,
 		`"baz" AND "qux"`,
@@ -16,23 +33,23 @@ func TestEvalostic(t *testing.T) {
 		`"1" AND NOT "2"`,
 		`"1" AND NOT "2"`,
 	})
-	assert.NoError(err)
-	assert.ElementsMatch(e.Match("foo"), []int{0})
-	assert.ElementsMatch(e.Match("bar"), []int{0})
-	assert.ElementsMatch(e.Match("foo bar"), []int{0})
-	assert.ElementsMatch(e.Match("baz"), nil)
-	assert.ElementsMatch(e.Match("baz qux"), []int{1})
-	assert.ElementsMatch(e.Match("qux baz"), []int{1})
-	assert.ElementsMatch(e.Match("ab"), nil)
-	assert.ElementsMatch(e.Match("ac"), []int{2})
-	assert.ElementsMatch(e.Match("ad"), []int{2})
-	assert.ElementsMatch(e.Match("bc"), []int{2})
-	assert.ElementsMatch(e.Match("bd"), []int{2})
-	assert.ElementsMatch(e.Match("cd"), nil)
-	assert.ElementsMatch(e.Match("abcd"), []int{2})
-	assert.ElementsMatch(e.Match("1"), []int{3, 4})
-	assert.ElementsMatch(e.Match("2"), nil)
-	assert.ElementsMatch(e.Match("12"), nil)
+	assert(t, err == nil)
+	assert(t, sameIntegers(e.Match("foo"), []int{0}))
+	assert(t, sameIntegers(e.Match("bar"), []int{0}))
+	assert(t, sameIntegers(e.Match("foo bar"), []int{0}))
+	assert(t, sameIntegers(e.Match("baz"), []int{}))
+	assert(t, sameIntegers(e.Match("baz qux"), []int{1}))
+	assert(t, sameIntegers(e.Match("qux baz"), []int{1}))
+	assert(t, sameIntegers(e.Match("ab"), []int{}))
+	assert(t, sameIntegers(e.Match("ac"), []int{2}))
+	assert(t, sameIntegers(e.Match("ad"), []int{2}))
+	assert(t, sameIntegers(e.Match("bc"), []int{2}))
+	assert(t, sameIntegers(e.Match("bd"), []int{2}))
+	assert(t, sameIntegers(e.Match("cd"), []int{}))
+	assert(t, sameIntegers(e.Match("abcd"), []int{2}))
+	assert(t, sameIntegers(e.Match("1"), []int{3, 4}))
+	assert(t, sameIntegers(e.Match("2"), []int{}))
+	assert(t, sameIntegers(e.Match("12"), []int{}))
 }
 
 func ExampleMatch() {
--- go.mod
+++ go.mod
@@ -2,7 +2,4 @@ module github.com/Codehardt/go-evalostic
 
 go 1.13
 
-require (
-	github.com/Codehardt/go-ahocorasick v0.0.0-20200817171610-7e9649e50ff9
-	github.com/stretchr/testify v1.6.1
-)
+require github.com/Codehardt/go-ahocorasick v1.0.1
